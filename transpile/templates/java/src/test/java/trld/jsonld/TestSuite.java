a74a6a63e70fc26efabca437d8b02112d354dfb1
Niklas Lindström <lindstream@gmail.com>
2020-10-24 15:30:50 +0200

    Fix test runners for Java and Javascript

--- transpile/templates/java/src/test/java/trld/jsonld/TestSuite.java
+++ transpile/templates/java/src/test/java/trld/jsonld/TestSuite.java
@@ -12,12 +12,15 @@ import static trld.jsonld.Base.CONTEXT;
 public class TestSuite {
 
     static final String TESTSUITE_DIR = "file://" + System.getProperties().get("user.home") + "/repos/github/w3c/json-ld-api/tests/";
-    static final String TESTSUITE_BASE_URL = "https://w3c.github.io/json-ld-api/tests";
+    static final String TESTSUITE_BASE_URL = "https://w3c.github.io/json-ld-api/tests/";
 
     static List<Arguments> testCaseProvider(String manifestFile) {
         List<Arguments> cases = new ArrayList<>();
         Map manifest = (Map) Common.loadJson(TESTSUITE_DIR + manifestFile);
         for (Map tc : (List<Map>) manifest.get("sequence")) {
+            if (!((List) tc.get("@type")).contains("jld:PositiveEvaluationTest"))
+                continue; // TODO: add handling of negative tests!
+
             cases.add(Arguments.of(((String) tc.get("@id")),
                                    ((String) tc.get("purpose")), tc));
         }
@@ -35,19 +38,22 @@ public class TestSuite {
     @ParameterizedTest(name = "Expand {0} - {1}")
     @MethodSource("expandTestCases")
     void expandTestSuite(String id, String purpose, Map tc) {
-        if (!((List) tc.get("@type")).contains("jld:PositiveEvaluationTest"))
-            return;
-
         String src = TESTSUITE_DIR + (String) tc.get("input");
         Map<String, Object> options = (Map) tc.getOrDefault("option", new HashMap<>());
         String baseUri = (String) options.getOrDefault("base", src);
+        boolean ordered = true;
+
+        String expandContext = (String) options.get("expandContext");
+        if (expandContext != null) {
+            expandContext = TESTSUITE_DIR + expandContext;
+        }
 
         Object sourceData = Common.loadJson(src);
 
         String expectedSrc = TESTSUITE_DIR + (String) tc.get("expect");
         Object expectedData = Common.loadJson(expectedSrc);
 
-        Object resultData = Expansion.expand(sourceData, baseUri);
+        Object resultData = Expansion.expand(sourceData, baseUri, expandContext, ordered);
 
         assertEquals(expectedData, resultData);
     }
@@ -55,9 +61,6 @@ public class TestSuite {
     @ParameterizedTest(name = "Compact {0} - {1}")
     @MethodSource("compactTestCases")
     void compactTestSuite(String id, String purpose, Map tc) {
-        if (!((List) tc.get("@type")).contains("jld:PositiveEvaluationTest"))
-            return;
-
         String src = TESTSUITE_DIR + (String) tc.get("input");
         String url = TESTSUITE_BASE_URL + (String) tc.get("input");
         Map<String, Object> options = (Map) tc.getOrDefault("option", new HashMap<>());
@@ -66,7 +69,7 @@ public class TestSuite {
         boolean ordered = true;
 
         Object sourceData = Common.loadJson(src);
-        sourceData = Expansion.expand(sourceData, baseUri);
+        sourceData = Expansion.expand(sourceData, baseUri, null, ordered);
         String contextSrc = TESTSUITE_DIR + (String) tc.get("context");
         Object contextData = Common.loadJson(contextSrc);
 
--- transpile/templates/js/test/test_suite.js
+++ transpile/templates/js/test/test_suite.js
@@ -3,51 +3,63 @@ import os from 'os'
 
 import { expand } from '../lib/jsonld/expansion.js'
 import { compact } from '../lib/jsonld/compaction.js'
-import { loadJson } from '../lib/jsonld/common'
+import * as common from '../lib/jsonld/common'
 
 const TESTSUITE_DIR = `file://${os.homedir()}/repos/github/w3c/json-ld-api/tests/`
-const TESTSUITE_BASE_URL = 'https://w3c.github.io/json-ld-api/tests'
+const TESTSUITE_BASE_URL = 'https://w3c.github.io/json-ld-api/tests/'
+
+let loadJson = common.loadJson
+function loadLocalJson(url) {
+    return loadJson(url.replace(TESTSUITE_BASE_URL, TESTSUITE_DIR))
+}
+common.loadJson = loadLocalJson
 
 function testCaseRunner(category) {
-  let manifestFile = `${TESTSUITE_DIR }${category}-manifest.jsonld`
-  let manifest = loadJson(manifestFile);
+  let manifestFile = `${TESTSUITE_DIR}${category}-manifest.jsonld`
+  let manifest = common.loadJson(manifestFile);
   for (let tc of manifest.sequence) {
-    if (tc["@type"].indexOf("jld:PositiveEvaluationTest") === -1) {
-      return
+    if (tc['@type'].indexOf('jld:PositiveEvaluationTest') === -1) {
+      continue
     }
-    let label = `${category} ${tc["@id"]} - ${tc.purpose}`
+    let label = `${category} ${tc['@id']} - ${tc.purpose}`
     tape.test(label, t => {
       let src = TESTSUITE_DIR + tc.input
       let url = TESTSUITE_BASE_URL + tc.input
       let options = tc.option || {}
       let baseUri = options.base || url
 
-      let sourceData = loadJson(src)
+      let ordered = true
+
+      let sourceData = common.loadJson(src)
 
       let contextData
       if (tc.context) {
         let contextSrc = TESTSUITE_DIR + tc.context
-        contextData = loadJson(contextSrc)
-        sourceData = expand(sourceData, baseUri)
+        contextData = common.loadJson(contextSrc)
+        sourceData = expand(sourceData, baseUri, null, ordered)
       }
 
       let expectedSrc = TESTSUITE_DIR + tc.expect
-      let expectedData = loadJson(expectedSrc)
+      let expectedData = common.loadJson(expectedSrc)
       if ('@context' in expectedData) {
         delete expectedData['@context']
       }
 
       let resultData
       if (contextData) {
-
         let compactArrays = options.compactArrays
         if (compactArrays === void(0)) compactArrays = true
-        resultData = compact(contextData, sourceData, baseUri, compactArrays)
+        resultData = compact(contextData, sourceData, baseUri, compactArrays, ordered)
       } else {
-        resultData = expand(sourceData, baseUri)
+        let expandContext = options.expandContext
+        if (expandContext) {
+            expandContext = TESTSUITE_DIR + expandContext
+        }
+        resultData = expand(sourceData, baseUri, expandContext, ordered)
       }
 
       t.deepEqual(resultData, expectedData)
+      //t.equal(JSON.stringify(resultData, null, 2), JSON.stringify(expectedData, null, 2))
       t.end()
     })
   }
