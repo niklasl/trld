a1c5967d9ac7f72642735c826ebd37c2c66f01ee
Niklas Lindström <lindstream@gmail.com>
2021-12-17 12:35:44 +0100

    Add annotation and changeset output variants

--- trld/diffld/__init__.py
+++ trld/diffld/__init__.py
@@ -1,21 +1,39 @@
-from typing import List, Dict, Iterable, NamedTuple, Optional, cast
+from typing import Any, List, Dict, Iterable, NamedTuple, Optional, cast
 import re
 
-#from ..jsonld.base import CONTEXT, GRAPH, ID, TYPE
-CONTEXT = '@context'
-GRAPH = '@graph'
-ID = '@id'
-TYPE = '@type'
+from ..common import dump_json, parse_json
+from ..jsonld.base import CONTEXT, GRAPH, ID, TYPE, VALUE, LIST, SET
+#CONTEXT = '@context'
+#GRAPH = '@graph'
+#ID = '@id'
+#TYPE = '@type'
+#VALUE = '@value'
+#LIST = '@list'
+#SET = '@set'
+ANNOTATION = '@annotation'
 
 
 GEN_BNODE_PFX = '_:diffnode-'
 CLEAN_BNODE = re.compile(r'[^A-Za-z0-9-_]')
+
+DIFF_CONTEXT = {
+        "nodemap": {"@id": "@graph", "@container": "@id"},
+        "diffs": {"@id": "diffs", "@container": ["@graph", "@id"]},
+    }
+
+CHANGESET_CONTEXT = dict(DIFF_CONTEXT, **{
+    'diff': 'https://niklasl.github.io/diff-id/ns#',
+})
 
 
 class DiffResult(NamedTuple):
     in_both: List
     in_a: Dict
     in_b: Dict
+
+
+def clone(o: object):
+    return parse_json(dump_json(o))
 
 
 def some_id(node: Dict, path: List[str]) -> str:
@@ -26,40 +44,153 @@ def some_id(node: Dict, path: List[str]) -> str:
     return cast(str, node[ID])
 
 
-def diffld(a: Dict, b: Dict) -> Dict:
+def make_changeset(graphs: List[Dict]) -> Dict:
+    context = cast(Dict, graphs[0]).get(CONTEXT)
+    seq: List[Dict] = []
+    result = [
+        {ID: "changeset", TYPE: 'diff:GraphChangeSet', 'diff:graphSequence': {LIST: seq}}
+    ]
+
+    def add_change(gid, added, itemmap, initial=None):
+        changeid = gid + (';removed' if not added else ';added')
+        seq.append({ID: changeid})
+        result.append({
+            ID: changeid,
+            TYPE: 'diff:NegativeGraph' if not added else 'diff:Graph',
+            GRAPH: (initial or []) + list(node
+                for node in itemmap.values()
+                if ID not in node or len(node) > 1),
+        })
+
+    for i, (a, b) in enumerate(zip(graphs[0:-1], graphs[1:])):
+        diff = diff_graphs(a, b)
+
+        if i == 0:
+            add_change(a[ID], True, diff.in_a, diff.in_both)
+
+        add_change(b[ID], True, diff.in_b)
+        add_change(b[ID], False, diff.in_a)
+
+    return {
+        CONTEXT: context,
+        GRAPH: result,
+    }
+
+
+def make_annotated(graphs: List[Dict]) -> Dict:
+    context = cast(Dict, graphs[0]).get(CONTEXT)
+
+    merged: Dict[str, object] = {}
+
+    def annotate_values(annot, nodes):
+        for node in nodes:
+            if ID in node and len(node) == 1:
+                continue
+
+            mnode = merged.setdefault(node[ID], {})
+            for key, value in node.items():
+                if key == ID:
+                    mnode[ID] = value
+                    continue
+
+                values = _aslist(mnode.get(key))
+                for value in _aslist(value):
+                    current = None
+
+                    if not isinstance(value, Dict):
+                        vkey = SET if key == TYPE else VALUE
+                        for some in values:
+                            if isinstance(some, Dict) and some.get(vkey) == value:
+                                current = some
+
+                        if current is None:
+                            value = {vkey: value}
+                            values.append(value)
+                        else:
+                            value = current
+                    else:
+                        if ID in value:
+                            for some in values:
+                                if isinstance(some, Dict) and some.get(ID) == value[ID]:
+                                    current = some
+                        if current is None:
+                            value = dict(value)
+                            values.append(value)
+                        else:
+                            value = current
+
+                    existing_annot = value.setdefault(ANNOTATION, {})
+                    # TODO: to avoid overwriting existing changes (due to bug below)
+                    new_annot = dict(annot)
+                    new_annot.update(existing_annot)
+                    existing_annot.update(new_annot)
+                    # TODO: fix regular diff of sets (shows as remove then re-add)
+                    if existing_annot.get('removedIn') == existing_annot.get('addedIn'):
+                        existing_annot.pop('removedIn', None)
+
+                mnode[key] = values
+
+
+    for i, (a, b) in enumerate(zip(graphs[0:-1], graphs[1:])):
+        diff = diff_graphs(a, b)
+
+        if i == 0:
+            annotate_values({'addedIn': {ID: a[ID]}},
+                            diff.in_both + list(diff.in_a.values()))
+
+        annotate_values({'addedIn': {ID: b[ID]}}, diff.in_b.values())
+        annotate_values({'removedIn': {ID: b[ID]}}, diff.in_a.values())
+
+    nodes = list(v for k, v in merged.items() if not k.startswith(GEN_BNODE_PFX))
+    turtlify(nodes, merged)
+
+    return {
+        CONTEXT: context,
+        GRAPH: nodes,
+    }
+
+
+def make_diff(a: Dict, b: Dict) -> Dict:
     a_id = cast(str, a.get(ID, '_:diffgraph-a'))
     b_id = cast(str, b.get(ID, '_:diffgraph-b'))
 
-    result = DiffResult(a[GRAPH], {}, {})
-    diff_nodes(result, a, b)
-
-    diff_context: object = {
-        "nodemap": {"@id": "@graph", "@container": "@id"},
-        "diffs": {"@id": "diffs", "@container": ["@graph", "@id"]}
-    }
+    diff = diff_graphs(a, b)
 
     given_context: Optional[object] = a.get(CONTEXT)
     assert given_context == b.get(CONTEXT)
+
+    context: object = DIFF_CONTEXT
     if given_context:
         if not isinstance(given_context, List):
             given_context = [given_context]
-        diff_context = given_context + [diff_context]
+        context = given_context + [DIFF_CONTEXT]
 
     return {
-        CONTEXT: diff_context,
-        "nodemap": to_nodemap(result.in_both),
+        CONTEXT: context,
+        "nodemap": to_nodemap(diff.in_both),
         "diffs": {
             a_id: {
-                "nodemap": to_nodemap(result.in_a.values())
+                "nodemap": to_nodemap(diff.in_a.values())
             },
             b_id: {
-                "nodemap": to_nodemap(result.in_b.values())
+                "nodemap": to_nodemap(diff.in_b.values())
             }
         }
     }
 
 
-def diff_nodes(result, a_node: object, b_node: object, path: List = None) -> bool:
+def diff_graphs(a: Dict, b: Dict) -> DiffResult:
+    # FIXME: fix mutation below!
+    a = clone(a)
+    b = clone(b)
+
+    diff = DiffResult(a[GRAPH], {}, {})
+    _diff_nodes(diff, a, b)
+
+    return diff
+
+
+def _diff_nodes(result, a_node: object, b_node: object, path: List = None) -> bool:
     path = [] if path is None else path + []
     equal = True
 
@@ -87,7 +218,7 @@ def diff_nodes(result, a_node: object, b_node: object, path: List = None) -> boo
             else:
                 a_child = a_node[i]
                 b_child = b_node[i]
-                if not diff_nodes(result, a_child, b_child, i_path):
+                if not _diff_nodes(result, a_child, b_child, i_path):
                     equal = False
 
     elif isinstance(a_node, Dict):
@@ -128,7 +259,7 @@ def diff_nodes(result, a_node: object, b_node: object, path: List = None) -> boo
                 equal = False
 
             if equal:
-                equal = diff_nodes(result, a_val, b_val, path + [key])
+                equal = _diff_nodes(result, a_val, b_val, path + [key])
 
             if not equal and key != ID and GRAPH not in a_node:
                 if key in a_node:
@@ -152,6 +283,7 @@ def diff_nodes(result, a_node: object, b_node: object, path: List = None) -> boo
             if not a_node_diff_added:
                 _reembed(a_node, result.in_a)
             equal = True
+
         if not a_node_diff_added and b_nid.startswith(GEN_BNODE_PFX):
             if not b_node_diff_added:
                 _reembed(b_node, result.in_b)
@@ -178,3 +310,22 @@ def to_nodemap(nodes: Iterable[Dict]) -> Dict:
             nodemap[node.pop(ID)] = node
 
     return nodemap
+
+
+def turtlify(o, nodemap):
+    if isinstance(o, List):
+        for x in o:
+            turtlify(x, nodemap)
+
+    if not isinstance(o, Dict):
+        return
+
+    for y in o.values():
+        turtlify(y, nodemap)
+
+    if ID in o and o[ID].startswith(GEN_BNODE_PFX):
+        _reembed(o, nodemap)
+
+
+def _aslist(o: object):
+    return o if isinstance(o, List) else [] if o is None  else [o]
--- trld/diffld/__main__.py
+++ trld/diffld/__main__.py
@@ -1,38 +1,65 @@
 import argparse
+import re
 from ..common import load_json, dump_json
-from . import *
+from . import CONTEXT, GRAPH, ID, TYPE,  make_diff, make_changeset, make_annotated
 
 
-def name_graph(data, iri):
-    if GRAPH in data and ID not in data:
-        data[ID] = iri
+def load_as_graph(docpath, embed_instance=False):
+    data = load_json(docpath)
+
+    if GRAPH not in data:
+        data = {GRAPH: [data]}
+
+    if ID not in data:
+        data[ID] = docpath.rsplit('.', 1)[0]
+
+    if embed_instance:
+        # Move local work into instance as a bnode:
+        if len(data[GRAPH]) > 2 and 'instanceOf' in data[GRAPH][1]:
+            ref_id = data[GRAPH][1]['instanceOf'].get(ID)
+            if ref_id == data[GRAPH][2].get(ID):
+                data[GRAPH][1]['instanceOf'] = data[GRAPH].pop(2)
+                data[GRAPH][1]['instanceOf'].pop(ID)
+
+    return data
 
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('version_a')
-argparser.add_argument('version_b')
+argparser.add_argument('first_version')
+argparser.add_argument('version', nargs='+')
+argparser.add_argument('-c', '--changeset', action='store_true')
+argparser.add_argument('-a', '--annotated', action='store_true')
 argparser.add_argument('-C', '--no-context', action='store_true')
 argparser.add_argument('-i', '--embed-instance', action='store_true')
 
 args = argparser.parse_args()
 
-a_data = load_json(args.version_a)
-b_data = load_json(args.version_b)
+assert not args.changeset or not args.annotated
 
-name_graph(a_data, args.version_a)
-name_graph(b_data, args.version_b)
+graphs = [load_as_graph(docpath, args.embed_instance)
+            for docpath in [args.first_version] + args.version]
 
-if args.embed_instance:
-    # Move local work into instance as a bnode:
-    if len(a_data[GRAPH]) > 2 and 'instanceOf' in a_data[GRAPH][1]:
-        ref_id = a_data[GRAPH][1]['instanceOf'].get(ID)
-        if ref_id == a_data[GRAPH][2].get(ID):
-            a_data[GRAPH][1]['instanceOf'] = a_data[GRAPH].pop(2)
-            a_data[GRAPH][1]['instanceOf'].pop(ID)
+if args.annotated:
+    diffresult = make_annotated(graphs)
+    records = []
+    for g in graphs:
+        record = g[GRAPH][0]
+        modified = record.get('modified')
+        agent = record.get('descriptionLastModifier')
+        if modified:
+            version = {ID: g[ID], TYPE: 'Record', 'modified': modified, 'agent': agent}
+            records.append(version)
 
-diffresult = diffld(a_data, b_data)
+    if records:
+        diffresult[GRAPH] = records + diffresult[GRAPH]
 
-if args.no_context:
+
+elif len(graphs) == 2 and not args.changeset:
+    diffresult = make_diff(graphs[0], graphs[1])
+else:
+    diffresult = make_changeset(graphs)
+
+if args.no_context and CONTEXT in diffresult:
     diffresult.pop(CONTEXT)
 
 s = dump_json(diffresult, pretty=True)
