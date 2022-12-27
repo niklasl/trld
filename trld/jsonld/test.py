dba778014653bf1d59bb8a6a33026ab987664e1d
Niklas Lindström <lindstream@gmail.com>
2022-12-27 13:34:12 +0100

    Fix local_testsuite_loader in trld.jsonld.test

--- trld/jsonld/test.py
+++ trld/jsonld/test.py
@@ -3,8 +3,7 @@ import sys
 import traceback
 from pathlib import Path
 
-from ..platform.io import Input
-from .docloader import set_document_loader
+from .docloader import load_any_document, set_document_loader
 from .base import JSONLD10, JSONLD11
 from . import context
 from .testbase import TestCase, TESTS_URL
@@ -31,7 +30,7 @@ def run_testsuite(suitefilepath: str) -> bool:
     suite_file_dir = _find_suite_file_dir(suitedir)
 
     def local_testsuite_loader(url, options=None):
-        return Input(url.replace(TESTS_URL, suite_file_dir))
+        return load_any_document(url.replace(TESTS_URL, suite_file_dir))
 
     set_document_loader(local_testsuite_loader)
 
