dad13c5980124754fa5c85c229c72f43501152fd
Niklas Lindström <lindstream@gmail.com>
2023-05-20 12:37:47 +0200

    Use crude trig serializer tests

--- Makefile
+++ Makefile
@@ -30,6 +30,7 @@ pytest: | cache/json-ld-api cache/trig-tests
 	python3 -m trld.jsonld.test cache/json-ld-api/tests/expand-manifest.jsonld cache/json-ld-api/tests/compact-manifest.jsonld cache/json-ld-api/tests/flatten-manifest.jsonld cache/json-ld-api/tests/fromRdf-manifest.jsonld cache/json-ld-api/tests/toRdf-manifest.jsonld 2>&1 | grep '^Running test suite\|^Ran '
 	python3 -m trld.tvm.test
 	python3 -m trld.trig.test | grep '^Ran '
+	python3 -m trld.trig.test_serializer | grep '^Examined '
 	python3 -m trld test/data/examples/misc.trig | python3 -m trld -i jsonld -o trig | python3 -m trld -i trig > /dev/null
 	python3 -m trld test/data/newlinestrings.jsonld -o ttl | python3 -m trld -i ttl -o ttl | python3 -m trld -i ttl > /dev/null
 
---
+++ trld/trig/test_serializer.py
@@ -0,0 +1,66 @@
+from io import StringIO
+from typing import Dict, Union, cast
+from pathlib import Path
+from ..platform.io import Input, Output
+from ..jsonld.base import CONTEXT
+
+from . import parser
+from . import serializer
+from .test import read_manifest, datarepr
+
+
+def run_tests(test_suite_dir: Union[str, Path]):
+    test_suite_dir = Path(test_suite_dir)
+
+    i = 0
+    failed = 0
+    rtrippable = 0
+    rtripped = 0
+
+    manifest = read_manifest(test_suite_dir / 'manifest.ttl')
+
+    for ttype, taction, tresult in manifest:
+        i += 1
+        trig_path = test_suite_dir / taction
+        negative = ttype in ('rdft:TestTrigNegativeSyntax', 'rdft:TestTrigNegativeEval')
+        if negative:
+            continue
+
+        inp = Input(str(trig_path))
+        try:
+            result = cast(Dict, parser.parse(inp))
+            assert result is not None
+        except Exception as e:
+            print(f'FAILED on {trig_path} (a {ttype}):', e)
+            failed += 1
+        else:
+            base_uri = 'http://www.w3.org/2013/TriGTests/'
+            context = result[CONTEXT]
+            resultrepr = datarepr(result, context, f'{base_uri}{trig_path.name}')
+
+            rtrippable += 1
+            out = Output()
+            serializer.serialize(result, out)
+            try:
+                rtrip = cast(Dict, parser.parse(Input(StringIO(out.get_value()))))
+            except Exception as e:
+                print(f'ERROR on {trig_path} (a {ttype}): {e!r}')
+                failed += 1
+            else:
+                rtripexpr = datarepr(rtrip, context, f'{base_uri}{trig_path.name}')
+                if resultrepr == rtripexpr:
+                    rtripped += 1
+                else:
+                    print('UNEQUAL:', resultrepr, '!=', rtripexpr)
+
+    print(f'Examined {i} tests.',
+          f'Round-tripped {rtrippable}, passed {rtripped}, failed {failed}')
+
+
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[1:]
+
+    test_suite_dir = args.pop(0) if args else 'cache/trig-tests'
+
+    run_tests(test_suite_dir)
