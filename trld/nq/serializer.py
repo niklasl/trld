d43b48d794466a62d6dda53f72f4e97365b5cb2f
Niklas Lindström <lindstream@gmail.com>
2026-05-18 21:08:55 +0200

    Serialize valid and canonical N-quads

--- trld/nq/serializer.py
+++ trld/nq/serializer.py
@@ -27,6 +27,7 @@ def repr_quad(triple: RdfTriple, graph_name: Optional[str]) -> str:
 
 
 def repr_term(t: RdfObject) -> str:
+    # NOTE: according to <https://www.w3.org/TR/rdf-canon/#canonical-quads>
     if isinstance(t, str):
         if is_blank(t):
             return t
@@ -35,8 +36,28 @@ def repr_term(t: RdfObject) -> str:
     else:
         assert isinstance(t, RdfLiteral)
         v: str = t.value
+        # Characters BS (backspace, code point U+0008), HT (horizontal tab,
+        # code point U+0009), LF (line feed, code point U+000A), FF (form feed,
+        # code point U+000C), CR (carriage return, code point U+000D), "
+        # (quotation mark, code point U+0022), and \ (backslash, code point
+        # U+005C) MUST be encoded using ECHAR.
         v = v.replace('\\', r'\\')
         v = v.replace('"', r'\"')
+        v = v.replace('\b', r'\b')
+        v = v.replace('\t', r'\t')
+        v = v.replace('\n', r'\n')
+        v = v.replace('\f', r'\f')
+        v = v.replace('\r', r'\r')
+
+        # Characters in the range from U+0000 to U+0007, VT (vertical tab, code
+        # point U+000B), characters in the range from U+000E to U+001F, DEL
+        # (delete, code point U+007F), and characters not matching the Char
+        # production from [XML11] MUST be represented by UCHAR using a
+        # lowercase \u with 4 HEXes.
+        v = ''.join(fr"\u{ord(c):04X}" if _needs_unicode_esc(c) else c for c in v)
+
+        # All characters not required to be represented by ECHAR or UCHAR MUST
+        # be represented by their native [UNICODE] representation.
         v = f'"{v}"'
         if t.language is not None:
             return f'{v}@{t.language}'
@@ -45,3 +66,19 @@ def repr_term(t: RdfObject) -> str:
             return f'{v}^^{dt}'
         else:
             return v
+
+
+def _needs_unicode_esc(c: str) -> bool:
+    cp = ord(c)
+    return (
+        (0x00 < cp <= 0x07)
+        or cp == 0x0B
+        or (0x0E < cp <= 0x1F)
+        or cp == 0x7F
+        or not (
+            # Char from <https://www.w3.org/TR/xml11/#charsets>
+            (0x01 < cp <= 0xD7FF)
+            or (0xE000 < cp <= 0xFFFD)
+            or (0x10000 < cp <= 0x10FFFF)
+        )
+    )
